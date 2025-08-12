5d77b81198a0f8b7296b4724d9d46d8b7c32cdc1
unknown <mdfaruk01720875872>
2025-08-12 12:27:14 +0600

    past deyloy netlypy

--- src/compnents/Projects.jsx
+++ src/compnents/Projects.jsx
@@ -12,7 +12,7 @@ const Projects = () => {
             </p>
             <div className="max-w-[90%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 ">
                {/* 1 project */}
-                <div className="bg-white rounded-lg shadow-md overflow-hidden flex flex-col">
+                <div className="bg-white rounded-lg  overflow-hidden flex flex-col shadow-xl hover:shadow-2xl transition-shadow duration-300">
                     <img src={devProject} alt='devProject' className="w-full h-48 object-cover" />
                     <div className="p-5 flex flex-col flex-grow">
                         <h2 className="text-xl font-semibold mb-3 text-gray-800">DevForum - A Modern Discussion Platform</h2>
@@ -20,7 +20,7 @@ const Projects = () => {
                     </div>
                 </div>
                 {/* 2 project */}
-                <div className="bg-white rounded-lg shadow-md overflow-hidden flex flex-col">
+                <div className="bg-white rounded-lg overflow-hidden flex flex-col shadow-xl hover:shadow-2xl transition-shadow duration-300">
                     <img src={roommateProject} alt='Roommate Finder' className="w-full h-48 object-cover" />
                     <div className="p-5 flex flex-col flex-grow">
                         <h2 className="text-xl font-semibold mb-3 text-gray-800">Roommate Finder</h2>
@@ -28,7 +28,7 @@ const Projects = () => {
                     </div>
                 </div>
                   {/* 3 project */}
-                <div className="bg-white rounded-lg shadow-md overflow-hidden flex flex-col">
+                <div className="bg-white rounded-lg  overflow-hidden flex flex-col shadow-xl hover:shadow-2xl transition-shadow duration-300">
                     <img src={courseProject} alt='courseProject' className="w-full h-48 object-cover" />
                     <div className="p-5 flex flex-col flex-grow">
                         <h2 className="text-xl font-semibold mb-3 text-gray-800">Course Management System</h2>
